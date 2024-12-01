83bfdfc351661c471ff3204b11b383c4071dce96
mubar3 <khusnul.mubar@yahoo.com>
2024-12-01 18:30:35 +0700

    fix sertif

--- resources/views/dashboard/cetak/sertifikat.blade.php
+++ resources/views/dashboard/cetak/sertifikat.blade.php
@@ -56,7 +56,7 @@
             display: flex;
             justify-content: center;
             gap: 2rem; /* Jarak antar teks */
-            font-size: 1.2rem;
+            font-size: 1rem;
             color: #000;
             font-family: 'Times New Roman', Times, serif;
             font-weight: bold;
